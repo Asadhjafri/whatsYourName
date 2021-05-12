0400794d82c654d2f76263a0587ae9242dda8c49
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2021-05-12 15:54:20 -0400

    created event to capture what user type and store is as a value. After capturing users name, it gets displayed as the header.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
 function App() {
-  const [headingText, setHeadingText] = useState("Hello");
+  const [headingText, setHeadingText] = useState("");
   const [isMouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState("");
 
   function handleClick() {
-    setHeadingText("Submitted");
+    setHeadingText(name);
   }
 
   function handleMouseColor() {
@@ -16,10 +17,19 @@ function App() {
     setMouseOver(false);
   }
 
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello {headingText}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
       <button
         style={{ backgroundColor: isMouseOver ? "black" : "white" }}
         onClick={handleClick}
